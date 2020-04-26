b267bac459f802cd3f78f13a3e9d399ac4120d9a
史斌 <assasin0308@sina.com>
2020-04-26 12:00:33 +0800

    blueprint post request

--- demo2/views.py
+++ demo2/views.py
@@ -1,8 +1,11 @@
 from . import demo2_app
 from tools import render_result
+from flask import request
 
 
 # 这里开始你的业务逻辑 以下是测试 视图
-@demo2_app.route('/demo2')
+@demo2_app.route('/demo2',methods=['GET','POST'])
 def demo2():
+    if request.method == 'GET':
+        return render_result('need post request')
     return render_result('this is demo2 module of demo2 view')
