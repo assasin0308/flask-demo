62609ef1af0ccff8a67ce157b0e4e23b13834d07
史斌 <assasin0308@sina.com>
2020-04-26 11:09:11 +0800

    unittest assert

---
+++ test_demo.py
@@ -0,0 +1,22 @@
+import unittest
+from run import app
+import json
+
+class demo1Test(unittest.TestCase):
+    """构造单元测试案例"""
+    def test_demo1App_is_success(self):
+        """创建进行web请求的客户端 """
+        client = app.test_client()
+        # 利用客户端模拟发送请求
+        # result = client.post('/demo1/demo1',data={}) # 模拟发送post请求 参数统一封装在 data 字典格式
+        result = client.get('/demo1/demo1',data={})
+        json_data = result.data
+        resp = json.loads(json_data)
+
+        # 断言测试
+        self.assertIn('errcode',resp)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
