7a3fcf10d0e6a259a9708a0fea94388676007771
史斌 <assasin0308@sina.com>
2020-04-27 16:49:38 +0800

    ssdb

--- ssdb_conn.py
+++ ssdb_conn.py
@@ -1,3 +1,37 @@
- # pip install --upgrade pyssdb
-# import pyssdb
-# client = pyssdb.Client()
+# -*- coding:utf-8 -*-
+# @Time: 2020/4/27 16:29
+# @Author: assasin
+# @Email: assasin0308@sina.com
+# @File: ssdb_conn.py
+
+import os,sys
+from sys import stdin,stdout
+
+from SSDB import SSDB
+
+class SSDBHelper(object):
+
+    def __init__(self):
+        try:
+            self.ssdb = SSDB('127.0.0.1', 8888)
+        except Exception as e:
+            print(e)
+            sys.exit(0)
+
+    def  set(self,key,value):
+        self.ssdb.request('set',[key,value])
+
+    def get(self,key):
+        self.ssdb.request('get',[key])
+
+    def incr(self,key,incr_num):
+        self.ssdb.request('incr',[key,incr_num])
+
+    def decr(self,key,decr_num):
+        self.ssdb.request('decr',[key,decr_num])
+
+    def delete(self,key):
+        self.ssdb.request('del',[key])
+
+    """......此处省略一万个方法"""
+
